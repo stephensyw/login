8fab8001f3402ba2ab8158416937244bf68eb59d
Shek Yu Wong <stephensyw@gmail.com>
2025-02-11 03:31:27 -0500

    fixed url

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,15 @@ const jwt = require('jsonwebtoken')
 const PORT = process.env.PORT || 5000;
 
 const app = express()
-app.use(cors())
 app.use(express.json())
+
+const corsOptions = {
+    origin: "*", // Change this to your Netlify URL in production
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+};
+app.use(cors(corsOptions));
+
 
 const users = [
 	{
@@ -33,7 +40,7 @@ app.post('/api/register', (req, res) => {
 		password
 	})
 	
-	res.status(201).json({
+	return res.status(201).json({
 		message: "User successfully register."
 	})
 })
@@ -45,13 +52,13 @@ app.post('/api/login', (req, res) =>{
 	
 	if(user){
 		const token = jwt.sign({username}, "secret_key", {expiresIn: "5s"})
-		res.status(200).json({
+		return res.status(200).json({
 			token: token,
 			message: "Successfully logged in"
 		})
 	}
 	
-	res.status(401).json({
+	return res.status(401).json({
 		message: "Invalid credentials"
 	})
 })
